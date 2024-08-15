0064e72c67e35c02df1071f728a0136707850b57
jesperfjellin <jesperfjellin@gmail.com>
2024-08-16 00:18:52 +0200

    Updated version number for Pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sosilogikk',  
-    version='1.0.0',
+    version='1.0.1',
     license='GPLv3',
     author='Jesper Fjellin',
     author_email='jesperfjellin@gmail.com',
